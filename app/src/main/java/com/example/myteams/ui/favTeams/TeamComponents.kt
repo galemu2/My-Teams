1f8d3aa8bf00e81db8d3d0b4636b20bf8d603bb1
galemu00 <galemu00@galemu00-Nitro-AN515-51>
2022-06-06 12:29:49 -0400

    implement swipe to delete functionality

---
+++ app/src/main/java/com/example/myteams/ui/favTeams/TeamComponents.kt
@@ -0,0 +1,176 @@
+package com.example.myteams.ui.favTeams
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.example.myteams.R
+import com.example.myteams.ui.theme.basicTextColor
+import com.example.myteams.ui.theme.teamNameColor
+
+@Composable
+fun TeamJersey(modifier: Modifier = Modifier, teamJersey: String = "") {
+
+    val context = LocalContext.current
+
+    val placeHolder: Painter = painterResource(id = R.drawable.placeholder_jersey)
+    val description = stringResource(id = R.string.team_jersey)
+    val model = ImageRequest
+        .Builder(context)
+        .data(teamJersey)
+        .crossfade(true)
+        .build()
+
+
+    Box(
+        modifier = modifier
+            .size(100.dp)
+            .background(color = Color.Transparent)
+            .clip(
+                RoundedCornerShape(2.dp)
+            )
+            .border(
+                width = 1.dp,
+                color = Color.Transparent,
+                shape = RoundedCornerShape(1.dp)
+            ), contentAlignment = Alignment.Center
+
+    ) {
+
+        AsyncImage(
+            modifier = Modifier
+                .padding(4.dp)
+                .background(
+                    color = Color.Transparent
+                ),
+            model = model,
+            contentDescription = description,
+            placeholder = placeHolder,
+            fallback = placeHolder,
+            error = placeHolder,
+            contentScale = ContentScale.Fit
+        )
+    }
+}
+
+@Composable
+fun TeamBadge(modifier: Modifier = Modifier, badge: String = "") {
+
+    val context = LocalContext.current
+
+    val model = ImageRequest
+        .Builder(context)
+        .data(badge)
+        .crossfade(true)
+        .build()
+
+
+    val placeHolder = painterResource(id = R.drawable.placeholder_badge)
+    val description = stringResource(id = R.string.team_badge)
+
+
+    Box(
+        modifier = modifier
+            .size(100.dp)
+            .clip(shape = CircleShape)
+            .background(color = Color.Transparent),
+        contentAlignment = Alignment.Center
+    ) {
+
+        AsyncImage(
+            modifier = Modifier
+                .padding(8.dp)
+                .background(
+                    color = Color.Transparent
+                ),
+            model = model,
+            contentDescription = description,
+            placeholder = placeHolder,
+            fallback = placeHolder,
+            contentScale = ContentScale.Fit
+        )
+    }
+}
+
+@Composable
+fun TeamName(
+    modifier: Modifier = Modifier,
+    name: String = stringResource(id = R.string.team_name)
+) {
+
+    Text(
+        modifier = modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .border(width = 1.dp, color = Color.Gray, shape = RoundedCornerShape(4.dp))
+            .padding(4.dp),
+        text = name,
+        fontSize = 30.sp,
+        fontFamily = FontFamily.Serif,
+        maxLines = 1,
+        overflow = TextOverflow.Ellipsis,
+        color = teamNameColor
+    )
+}
+
+@Composable
+fun SportName(
+    modifier: Modifier = Modifier,
+    sportName: String = stringResource(id = R.string.sport_name)
+) {
+
+    Text(
+        modifier = modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(4.dp),
+        text = sportName,
+        maxLines = 1,
+        overflow = TextOverflow.Ellipsis,
+        fontFamily = FontFamily.SansSerif,
+        fontSize = 18.sp,
+        color = basicTextColor
+    )
+}
+
+@Composable
+fun LeagueName(
+    modifier: Modifier = Modifier,
+    leagueName: String = stringResource(id = R.string.league_name)
+) {
+    Text(
+        modifier = modifier
+            .wrapContentHeight()
+            .fillMaxWidth()
+            .padding(4.dp),
+        text = leagueName,
+        fontFamily = FontFamily.SansSerif,
+        maxLines = 1,
+        fontSize = 10.sp,
+        overflow = TextOverflow.Ellipsis,
+        color = basicTextColor,
+
+
+        )
+
+}
+
+
--- app/src/main/java/com/example/myteams/ui/favTeams/TeamsContent.kt
+++ app/src/main/java/com/example/myteams/ui/favTeams/TeamsContent.kt
@@ -7,38 +7,25 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.AlertDialog
-import androidx.compose.material.Button
+import androidx.compose.material.*
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
 import com.example.myteams.R
 import com.example.myteams.data.models.Team
 import com.example.myteams.data.models.Teams
 import com.example.myteams.ui.SportsTeamViewModel
-import com.example.myteams.ui.theme.basicTextColor
 import com.example.myteams.ui.theme.contentBackground
 import com.example.myteams.ui.theme.displayFavTeamBackground
-import com.example.myteams.ui.theme.teamNameColor
 import com.example.myteams.util.Resource
 
+@ExperimentalMaterialApi
 @ExperimentalMaterial3Api
 @Composable
 fun HandleTeamContent(
@@ -91,6 +78,7 @@ fun HandleTeamContent(
 
 }
 
+@ExperimentalMaterialApi
 @Composable
 fun DisplayFavTeams(
     favTeams: List<Team>,
@@ -107,93 +95,35 @@ fun DisplayFavTeams(
             items = favTeams,
             key = { team -> team.idTeam }
         ) { team ->
-            TeamItemFaves(
-                team = team,
-                viewModel = viewModel,
-                displayTeamHistory = {
-                    Toast.makeText(context, "id: $it", Toast.LENGTH_SHORT).show()
-                }
-            )
-        }
-    }
-}
 
-@Composable
-fun DisplaySearchTeams(
-    teams: List<Team>,
-    viewModel: SportsTeamViewModel,
-) {
-
-    LazyColumn(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(color = contentBackground),
-    ) {
-        items(
-            items = teams,
-            key = { team -> team.idTeam }
-        ) { team ->
-            TeamItemSearch(
-                team = team,
-                viewModel = viewModel
-            )
-        }
-    }
-}
-
-
-@Composable
-fun SelectFavTeamDialog(
-    openDialogState: Boolean,
-    updateDialogState: (Boolean) -> Unit,
-    insertToDatabase: () -> Unit,
-) {
-
-    if (openDialogState)
-        AlertDialog(
-            onDismissRequest = {
-                updateDialogState(false)
-            },
-            title = {
-                Text(text = stringResource(id = R.string.save_team))
-            },
-            text = {
-                Text(text = stringResource(id = R.string.save_selected_team))
-            },
-            buttons = {
-                Row(
-                    modifier = Modifier.padding(8.dp),
-                    horizontalArrangement = Arrangement.End
-                ) {
-                    MyButton(
-                        updateDialogState = updateDialogState,
-                        text = stringResource(id = R.string.cancel)
-                    ) {}
-                    MyButton(
-                        updateDialogState,
-                        text = stringResource(id = R.string.select),
-                        insertToDatabase = insertToDatabase
-                    )
-                }
+            val dismissState = rememberDismissState()
+            val dismissDirection = dismissState.dismissDirection
+            val isDismissed = dismissState.isDismissed(DismissDirection.EndToStart)
+            if (isDismissed && dismissDirection == DismissDirection.EndToStart) {
+                // delete from database
+                viewModel.deleteFavTeam(favTeam = team)
             }
-        )
-}
 
-@Composable
-private fun MyButton(
-    updateDialogState: (Boolean) -> Unit,
-    text: String,
-    insertToDatabase: () -> Unit
-) {
-    Button(
-        modifier = Modifier
-            .wrapContentSize()
-            .padding(8.dp),
-        onClick = {
-            updateDialogState(false)
-            insertToDatabase()
-        }) {
-        Text(text = text)
+
+
+            SwipeToDismiss(
+                state = dismissState,
+                directions = setOf(DismissDirection.EndToStart),
+                dismissThresholds = { FractionalThreshold(fraction = 0.3f) },
+                background = {}
+            ) {
+                TeamItemFaves(
+                    team = team,
+                    viewModel = viewModel,
+                    displayTeamHistory = {
+
+                        // todo used to navigate to team history
+                        Toast.makeText(context, "id: $it", Toast.LENGTH_SHORT).show()
+                    }
+                )
+            }
+
+        }
     }
 }
 
@@ -207,8 +137,6 @@ fun TeamItemFaves(
 ) {
 
     val context = LocalContext.current
-
-
 
     Row(
         modifier = Modifier
@@ -259,6 +187,31 @@ fun TeamItemFaves(
     }
 
 }
+
+
+@Composable
+fun DisplaySearchTeams(
+    teams: List<Team>,
+    viewModel: SportsTeamViewModel,
+) {
+
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = contentBackground),
+    ) {
+        items(
+            items = teams,
+            key = { team -> team.idTeam }
+        ) { team ->
+            TeamItemSearch(
+                team = team,
+                viewModel = viewModel
+            )
+        }
+    }
+}
+
 
 @Composable
 fun TeamItemSearch(
@@ -333,169 +286,58 @@ fun TeamItemSearch(
 }
 
 @Composable
-fun TeamJersey(modifier: Modifier = Modifier, teamJersey: String = "") {
+fun SelectFavTeamDialog(
+    openDialogState: Boolean,
+    updateDialogState: (Boolean) -> Unit,
+    insertToDatabase: () -> Unit,
+) {
 
-    val context = LocalContext.current
-
-    val placeHolder: Painter = painterResource(id = R.drawable.placeholder_jersey)
-    val description = stringResource(id = R.string.team_jersey)
-    val model = ImageRequest
-        .Builder(context)
-        .data(teamJersey)
-        .crossfade(true)
-        .build()
-
-
-
-
-    Box(
-        modifier = modifier
-            .size(100.dp)
-            .background(color = Color.Transparent)
-            .clip(
-                RoundedCornerShape(2.dp)
-            )
-            .border(
-                width = 1.dp,
-                color = Color.Transparent,
-                shape = RoundedCornerShape(1.dp)
-            ), contentAlignment = Alignment.Center
-
-    ) {
-
-        AsyncImage(
-            modifier = Modifier
-                .padding(4.dp)
-                .background(
-                    color = Color.Transparent
-                ),
-            model = model,
-            contentDescription = description,
-            placeholder = placeHolder,
-            fallback = placeHolder,
-            error = placeHolder,
-            contentScale = ContentScale.Fit
+    if (openDialogState)
+        AlertDialog(
+            onDismissRequest = {
+                updateDialogState(false)
+            },
+            title = {
+                Text(text = stringResource(id = R.string.save_team))
+            },
+            text = {
+                Text(text = stringResource(id = R.string.save_selected_team))
+            },
+            buttons = {
+                Row(
+                    modifier = Modifier.padding(8.dp),
+                    horizontalArrangement = Arrangement.End
+                ) {
+                    MyButton(
+                        updateDialogState = updateDialogState,
+                        text = stringResource(id = R.string.cancel)
+                    ) {}
+                    MyButton(
+                        updateDialogState,
+                        text = stringResource(id = R.string.select),
+                        insertToDatabase = insertToDatabase
+                    )
+                }
+            }
         )
+}
+
+
+@Composable
+private fun MyButton(
+    updateDialogState: (Boolean) -> Unit,
+    text: String,
+    insertToDatabase: () -> Unit
+) {
+    Button(
+        modifier = Modifier
+            .wrapContentSize()
+            .padding(8.dp),
+        onClick = {
+            updateDialogState(false)
+            insertToDatabase()
+        }) {
+        Text(text = text)
     }
 }
 
-@Composable
-fun TeamBadge(modifier: Modifier = Modifier, badge: String = "") {
-
-    val context = LocalContext.current
-
-    val model = ImageRequest
-        .Builder(context)
-        .data(badge)
-        .crossfade(true)
-        .build()
-
-
-    val placeHolder = painterResource(id = R.drawable.placeholder_badge)
-    val description = stringResource(id = R.string.team_badge)
-
-
-    Box(
-        modifier = modifier
-            .size(100.dp)
-            .clip(shape = CircleShape)
-            .background(color = Color.Transparent),
-        contentAlignment = Alignment.Center
-    ) {
-
-        AsyncImage(
-            modifier = Modifier
-                .padding(8.dp)
-                .background(
-                    color = Color.Transparent
-                ),
-            model = model,
-            contentDescription = description,
-            placeholder = placeHolder,
-            fallback = placeHolder,
-            contentScale = ContentScale.Fit
-        )
-    }
-}
-
-@Composable
-fun TeamName(
-    modifier: Modifier = Modifier,
-    name: String = stringResource(id = R.string.team_name)
-) {
-
-    Text(
-        modifier = modifier
-            .fillMaxWidth()
-            .wrapContentHeight()
-            .border(width = 1.dp, color = Color.Gray, shape = RoundedCornerShape(4.dp))
-            .padding(4.dp),
-        text = name,
-        fontSize = 30.sp,
-        fontFamily = FontFamily.Serif,
-        maxLines = 1,
-        overflow = TextOverflow.Ellipsis,
-        color = teamNameColor
-    )
-}
-
-@Composable
-fun SportName(
-    modifier: Modifier = Modifier,
-    sportName: String = stringResource(id = R.string.sport_name)
-) {
-
-    Text(
-        modifier = modifier
-            .fillMaxWidth()
-            .wrapContentHeight()
-            .padding(4.dp),
-        text = sportName,
-        maxLines = 1,
-        overflow = TextOverflow.Ellipsis,
-        fontFamily = FontFamily.SansSerif,
-        fontSize = 18.sp,
-        color = basicTextColor
-    )
-}
-
-@Composable
-fun LeagueName(
-    modifier: Modifier = Modifier,
-    leagueName: String = stringResource(id = R.string.league_name)
-) {
-    Text(
-        modifier = modifier
-            .wrapContentHeight()
-            .fillMaxWidth()
-            .padding(4.dp),
-        text = leagueName,
-        fontFamily = FontFamily.SansSerif,
-        maxLines = 1,
-        fontSize = 10.sp,
-        overflow = TextOverflow.Ellipsis,
-        color = basicTextColor,
-
-
-        )
-
-}
-
-/*
-@Preview(
-    showBackground = false,
-    name = "Night Mode",
-    uiMode = Configuration.UI_MODE_NIGHT_YES
-)
-@Preview(
-    showBackground = true,
-    name = "Day Mode",
-    uiMode = Configuration.UI_MODE_NIGHT_NO
-)
-@Composable
-fun PreviewTeamBadge() {
-    MyTeamsTheme {
-        SportName()
-    }
-}
- */
